1ece0df0267e58d3695e3e19ac745bfd149c8248
Eduardo Varela <014426634@ulsaoaxaca.edu.mx>
2023-05-06 17:41:56 -0600

    8 ~ Render PokemonCard

--- src/assets/styles/styles.js
+++ src/assets/styles/styles.js
@@ -8,5 +8,37 @@ export const styles = StyleSheet.create({
   },
   flatListContentContainer: {
     paddingHorizontal: 5,
+    marginTop: 65,
+  },
+  card: {
+    flex: 1,
+    height: 130,
+  },
+  spacing: {
+    flex: 1,
+    padding: 5,
+  },
+  bgStyles: {
+    backgroundColor: "grey",
+  },
+  number: {
+    position: "absolute",
+    right: 10,
+    top: 10,
+    color: "#fff",
+    fontSize: 11,
+  },
+  name: {
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 15,
+    paddingTop: 10,
+  },
+  image: {
+    position: "absolute",
+    bottom: 2,
+    right: 2,
+    width: 90,
+    height: 90,
   },
 });
