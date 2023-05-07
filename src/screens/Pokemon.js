ddf8f75939afb1fe3d1128114cf153688e19325e
Eduardo Varela <014426634@ulsaoaxaca.edu.mx>
2023-05-06 23:55:03 -0600

    "Consult pokemon by id"

--- src/screens/Pokemon.js
+++ src/screens/Pokemon.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Text, SafeAreaView } from "react-native";
-
+import { styles } from "../assets/styles/styles";
+import { getPokemonDetailsApi } from "../api/pokemon";
 export default function Pokemon(props) {
-  const { route } = props;
+  const {
+    navigation,
+    route: { params },
+  } = props;
+
+  const [pokemon, setPokemon] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await getPokemonDetailsApi(params.id);
+        setPokemon(response);
+      } catch (error) {
+        navigation.goBack();
+      }
+    })();
+  }, [params]);
+  if (!pokemon) return null;
   return (
-    <SafeAreaView>
-      <Text> Pokemon </Text>
+    <SafeAreaView style={styles.container}>
+      <Text>{pokemon.name}</Text>
     </SafeAreaView>
   );
 }
